a6147a01127c88a1da803f03170306793e9d4bab
jdssanil <sanilk@jdsindustries.com>
2023-08-25 16:28:03 -0500

    Added hightlight component and iconify

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,12 @@
+import React from "react";
+// Import the original mapper
+import MDXComponents from "@theme-original/MDXComponents";
+import { Icon } from "@iconify/react"; // Import the entire Iconify library.
+import Highlight from "../components/highlight";
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  icon: Icon, // Make the iconify Icon component available in MDX as <icon />.
+  Highlight,
+};
