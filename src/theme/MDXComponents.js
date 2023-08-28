379d9ea7266cd9a9c36823135c8d3e7c2ac5acdc
Sanil109 <sanilpsy@gmail.com>
2023-08-28 14:59:23 -0500

    Cleaned and added mrc docs

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,12 +1,12 @@
 import React from "react";
 // Import the original mapper
 import MDXComponents from "@theme-original/MDXComponents";
-import { Icon } from "@iconify/react"; // Import the entire Iconify library.
+import { Icon } from "@iconify/react";
 import Highlight from "../components/highlight";
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  icon: Icon, // Make the iconify Icon component available in MDX as <icon />.
+  icon: Icon,
   Highlight,
 };
